456a443c1e8856baef98fc5e4d85cfdfcaae80d8
David Jonathan <david.jonn1@hotmail.com>
2021-10-12 12:00:46 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoQuadrado/TelaPrincipal.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CalculoQuadrado
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelNumero = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Unispace", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTitulo.ForeColor = System.Drawing.Color.Black;
+            this.labelTitulo.Location = new System.Drawing.Point(82, 41);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(623, 33);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Calcular o valor quadrado de um número";
+            // 
+            // labelNumero
+            // 
+            this.labelNumero.AutoSize = true;
+            this.labelNumero.BackColor = System.Drawing.Color.Black;
+            this.labelNumero.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelNumero.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelNumero.Location = new System.Drawing.Point(211, 130);
+            this.labelNumero.Name = "labelNumero";
+            this.labelNumero.Size = new System.Drawing.Size(114, 15);
+            this.labelNumero.TabIndex = 1;
+            this.labelNumero.Text = "Digite um Número:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(331, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonCalcular.Location = new System.Drawing.Point(367, 168);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(90, 27);
+            this.buttonCalcular.TabIndex = 3;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = false;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResultado.Font = new System.Drawing.Font("Lucida Sans Unicode", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelResultado.Location = new System.Drawing.Point(197, 236);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(383, 97);
+            this.labelResultado.TabIndex = 5;
+            this.labelResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Black;
+            this.groupBox2.Location = new System.Drawing.Point(197, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(395, 107);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // TelaPrincipal
+            // 
+            this.AcceptButton = this.buttonCalcular;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelNumero);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo Quadrado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelNumero;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ CalculoQuadrado/TelaPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoQuadrado
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            int numero = 0;
+            int quadrado = 0;
+
+            numero = Convert.ToInt32(textBox1.Text);
+            quadrado = numero * numero;
+
+            labelResultado.Text = "Quadrado de " + numero + " é " + quadrado;
+
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
